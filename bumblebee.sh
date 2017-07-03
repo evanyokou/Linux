23c2be8c03035f42bfeeaf70bf7df9208de6b5aa
yokou <evanyokou@sina.com>
2017-07-03 15:54:03 +0800

    first time commit

---
+++ bumblebee.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt-add-repository ppa:bumblebee/stable
+sudo apt-get update
+sudo apt-get install bumblebee bumblebee-nvidia
+
---
+++ ubuntu-remove.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt-get autoremove --purge libreoffice-common
+sudo apt-get autoremove --purge unity-webapps-common
+sudo apt-get autoremove --purge thunderbird totem rhythmbox empathy brasero simple-scan gnome-mahjongg aisleriot gnome-mines cheese transmission-common gnome-orca webbrowser-app gnome-sudoku landscape-client-ui-install 
+sudo apt-get autoremove --purge onboard deja-dup
